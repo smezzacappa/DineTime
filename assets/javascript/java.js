6c0274237f4963e795f6da2c9dfd2ccc762da7d4
Daniel <danstraw86@gmail.com>
2018-03-19 19:01:22 -0700

    saved updates to files in correct directory

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,119 +1,81 @@
-// Food Search
-$("#search-food").on("click", function (event) {
-    event.preventDefault();
 
-    var food = $("#food-input").val().trim();
-    if (food !== "") {
-        $("#food-input").val("")
-        foodSearch(food);
-    }
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyDFxvI2pF2TVAL8YxlTKiIJsA3zAT8wT1I",
+authDomain: "dinetime-c2874.firebaseapp.com",
+databaseURL: "https://dinetime-c2874.firebaseio.com",
+projectId: "dinetime-c2874",
+storageBucket: "",
+messagingSenderId: "647476940046"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
-})  // End of $("#search-food").on("click", function (event) {}
+// function getSelectionType() {
+    
+// }
+console.log('test');
 
-
-// Drink Search
-$("#search-drink").on("click", function (event) {
-    event.preventDefault();
-
-    var drink = $("#drink-input").val().trim();
-    if (drink !== "") {
-        $("#drink-input").val("")
-        drinkSearch(drink);
-    }
-
-})  // End of $("#search-food").on("click", function (event) {}
-
-
-// Food search function
-function foodSearch(food) {
-    var API_KEY = "fb001d1c57dffa88545ebe6f986046e9	";
-    var APP_ID = "5f782d14";
-    var queryURL = "https://api.edamam.com/search?app_id=" + APP_ID + "&app_key=" + API_KEY + "&q=" + food;
+function getDrink() {
+    var userDrink = $("#drink-input").val().trim();
+    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + userDrink;
+    console.log(userDrink);
 
     $.ajax({
-        url: queryURL,
         method: 'GET',
-    }).then(function (response) {
-        $("#food-drink-view").empty();
-
-        for (var i = 0; i < response.hits.length; i++) {
-            var imageDiv = $('<div>');
-            imageDiv.addClass('imgClass');
-
-            var image = $("<img>");
-            image.attr("src", response.hits[i].recipe.image);
-            imageDiv.append(image);
-
-            var pOne = $("<p>").text("Label: " + response.hits[i].recipe.label);
-            imageDiv.append(pOne);
-
-            var pTwo = $("<p>").text("Ingredient: " + response.hits[i].recipe.ingredientLines);
-            imageDiv.append(pTwo);
-
-            var pThree = $("<p>").text("Calories: " + response.hits[i].recipe.calories);
-            imageDiv.append(pThree);
-
-            var pFour = $("<p>").text("Total Weight: " + response.hits[i].recipe.totalWeight);
-            imageDiv.append(pFour);
-
-            $("#food-drink-view").prepend(imageDiv);
+        url: queryURL,
+    }).then(function(response) {
+        var drinkObj = response.drinks[0]
+        console.log(drinkObj);
+        var ingredients = [];
+        for (let i = 9; i < 23; i++) {
+            var ingredient = drinkObj[Object.keys(drinkObj)[i]];
+            if (ingredient !== "") {
+                ingredients.push(ingredient);  
+            }     
         }
+        var measurements = [];
+        for (let i = 24; i < (ingredients.length + 24); i++){
+            measurements.push(drinkObj[Object.keys(drinkObj)[i]])  
+        }
+        var drinkName = response.drinks[0].strDrink;
+        var recipe = [];
+        database.ref().push( {
+            drink: drinkName,
+            ingredients: ingredients,
+            measurements: measurements,
+            picture: drinkObj.strDrinkThumb,
+        });
 
-
+        console.log(measurements);
     })
+}
+getDrink();
 
-}   //  End of function foodSearch(food){}
+database.ref().on("child_added", function(snapshot) {
+    var drinkDisplay = $("<div>");
+    drinkDisplay.attr({
+        "id": "drinkDisplay",
+    })
+    var ingredients = snapshot.val().ingredients;
+    var measurements = snapshot.val().measurements;
+    var recipe = $("<ul>");
+    recipe.attr("id", "recipe");
+    for (let i = 0; i < ingredients.length; i++) {
+        var recipeItem = $("<li>");
+        $(recipeItem).text(ingredients[i] + ": " + measurements[i]);
+        $(recipe).append(recipeItem);  
+    }
+    var picture = $("<img>");
+    picture.attr({
+        "src": snapshot.val().picture,
+        'id': "drinkPic",
+    });
+    var drink = $("<h4>");
+    drink.text(snapshot.val().drink);
+    $(drinkDisplay).append(drink);
+    $(drinkDisplay).append(recipe);
+    $(drinkDisplay).append(picture);
+    $("body").append(drinkDisplay);
 
-
-
-// Drink search function
-function drinkSearch(drink) {
-    var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink;
-
-    // API response function
-    $.ajax({
-        url: queryURL,
-        method: 'GET',
-    }).then(function (response) {
-        $("#food-drink-view").empty();
-        console.log(response)
-
-        for (var i = 0; i < response.drinks.length; i++) {
-            var imageDiv = $('<div>');
-            imageDiv.addClass('imgClass');
-
-            // Make an image div
-            var image = $("<img>");
-            image.attr("src", response.drinks[i].strDrinkThumb);
-            imageDiv.append(image);
-
-            var pOne = $("<p>").text("Drink-ID: " + response.drinks[i].idDrink);
-            imageDiv.append(pOne);
-
-            var pTwo = $("<p>").text("Drink Label: " + response.drinks[i].strDrink);
-            imageDiv.append(pTwo);
-
-            var pThree = $("<p>").text("Alcohol: " + response.drinks[i].strAlcoholic);
-            imageDiv.append(pThree);
-            var drinkObj = response.drinks[i]
-            for (var j = 9; j < 23; j++) {
-                var ingredient = drinkObj[Object.keys(drinkObj)[j]];
-                if (ingredient !== "") {
-                    var pFour = $("<p>").text("Ingredient: " + drinkObj[Object.keys(drinkObj)[j]]);
-                    imageDiv.append(pFour);
-                }
-            }
-            var pFive = $("<p>").text("Instruction: " + response.drinks[i].strInstructions);
-            imageDiv.append(pFive);
-            $("#food-drink-view").prepend(imageDiv);
-
-        }
-
-    })  // End of the response function
-
-}   //  End of function foodSearch(food){}
-
-
-
-
-
+});
